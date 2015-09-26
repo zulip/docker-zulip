dd74be87bb330985c8685a6ea24cab7ea4af4fa1
Alexander Trost <galexrt@googlemail.com>
2015-09-26 16:16:46 +0200

    Fixed the missing .database-initialized file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-if [ -f "" ]; then
+if [ ! -f "$ZULIP_DIR/.database-initialized" ]; then
     su zulip -c "$ZULIP_DIR/deployments/current/scripts/setup/initialize-database"
+    su zulip -c "touch $ZULIP_DIR/.database-initialized"
 fi
