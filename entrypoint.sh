de7b59b35261f6f4dba1adbb4c09766c4596221e
Sampriti Panda <sampdingo@hotmail.com>
2018-07-09 17:44:53 +0530

    entrypoint: Add call to zulip-puppet-apply for nginx config.
    First, we stop deleting the `puppet` directory, which is important for
    being able to run `zulip-puppet-apply` inside the container (super
    useful for debugging).
    
    Second, we call this inside the code block for the `nginx` option, in
    order to support the `DISABLE_HTTPS` option working without any copied
    nginx configuration code in docker-zulip.
    
    See #137 for how we could optimize this away.

--- entrypoint.sh
+++ entrypoint.sh
@@ -147,6 +147,7 @@ nginxConfiguration() {
     sed -i "s/worker_processes .*/worker_processes $NGINX_WORKERS;/g" /etc/nginx/nginx.conf
     sed -i "s/client_max_body_size .*/client_max_body_size $NGINX_MAX_UPLOAD_SIZE;/g" /etc/nginx/nginx.conf
     sed -i "s/proxy_buffering .*/proxy_buffering $NGINX_PROXY_BUFFERING;/g" /etc/nginx/zulip-include/proxy_longpolling
+    /home/zulip/deployments/current/scripts/zulip-puppet-apply -f
     echo "Nginx configuration succeeded."
 }
 configureCerts() {
