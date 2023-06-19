fa6062a0159cfaa6d266f3e8b7a909503afc12df
Mursith <nmursith@gmail.com>
2023-06-19 13:47:09 +0530

    Consider SOCIAL_AUTH_SAML_ENABLED_IDPS as array in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -290,6 +290,7 @@ zulipConfiguration() {
            [ "$setting_key" = "AUTH_LDAP_USER_FLAGS_BY_GROUP" ] || \
            [ "$setting_key" = "AUTH_LDAP_GROUP_TYPE" ] || \
            [ "$setting_key" = "SOCIAL_AUTH_OIDC_ENABLED_IDPS" ] || \
+           [ "$setting_key" = "SOCIAL_AUTH_SAML_ENABLED_IDPS" ] || \
            { [ "$setting_key" = "LDAP_APPEND_DOMAIN" ] && [ "$setting_var" = "None" ]; } || \
            [ "$setting_key" = "SECURE_PROXY_SSL_HEADER" ] || \
            [[ "$setting_key" = "CSRF_"* ]] || \
