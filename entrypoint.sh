fd2e1643afb488dd1dd49437369889ca51a96b4a
Alex Vandiver <alexmv@zulip.com>
2024-04-18 10:38:16 -0400

    Revert "entrypoint: Remove unnecessary use of an extended glob."
    This reverts commit 0d72546a4cb183c985de6d96873ac5508cfd3ce3.  The
    extended glob matched when the _entire_ value was numeric, whereas
    the pattern matches if it _starts_ with a number.
    
    Fixes: #433
    Fixes: #435

--- entrypoint.sh
+++ entrypoint.sh
@@ -89,7 +89,7 @@ setConfigurationValue() {
             [Tt][Rr][Uu][Ee]|[Ff][Aa][Ll][Ss][Ee]|[Nn]one)
             TYPE="bool"
             ;;
-            [1-9][0-9]*)
+            +([0-9]))
             TYPE="integer"
             ;;
             [\[\(]*[\]\)])
