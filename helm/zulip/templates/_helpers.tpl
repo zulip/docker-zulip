zulip/docker-zulip

commit ca0052022c1623f616838bd64f710a8a8c345d79

entrypoint: Use REMOTE_POSTGRES_*, rather than using DB_* env vars.
The `DB_HOST` and `DB_HOST_PORT` were added because they provided a
way for the entrypoint to know what the host/port to wait to come up
was.  However, we can achieve the same result using
get-django-setting, which will work even with
`MANUAL_CONFIGURATION=True`.  This removes one pair of "special"
variables.

The claimed need for different defaulting behaviour for
`SETTING_REMOTE_POSTGRES_SSLMODE` was incorrect, as the
`MANUAL_CONFIGURATION` checks only examine exported environment
variables (i.e. those explicitly set by the Docker runtime) and not
local shell variables.

helm/zulip/templates/_helpers.tpl
@@ -66,9 +66,9 @@ include all env variables for Zulip pods
 */}}
 {{- define "zulip.env" -}}
 
-- name: DB_HOST
+- name: REMOTE_POSTGRES_HOST
   value: "{{ template "postgresql.v1.primary.fullname" .Subcharts.postgresql }}"
-- name: DB_HOST_PORT
+- name: REMOTE_POSTGRES_PORT
   value: "{{ template "postgresql.v1.service.port" .Subcharts.postgresql }}"
 - name: SETTING_MEMCACHED_LOCATION
   value: "{{ template "common.names.fullname" .Subcharts.memcached }}:11211"

ci/manual_configuration/no-envs.yaml
@@ -2,9 +2,7 @@
 services:
   zulip:
     environment: !override
-      # The !override removes the default SETTING_ values.  We still need the
-      # DB_HOST so we know what database host to wait for
-      DB_HOST: database
+      # The !override removes the default SETTING_ values.
       MANUAL_CONFIGURATION: True
     volumes:
       - ./ci/manual_configuration/settings.py:/etc/zulip/settings.py

ci/manual_configuration/settings.py
@@ -3,7 +3,6 @@ MEMCACHED_LOCATION = "memcached:11211"
 RABBITMQ_HOST = "rabbitmq"
 REDIS_HOST = "redis"
 REMOTE_POSTGRES_HOST = "database"
-REMOTE_POSTGRES_PORT = 5432
 REMOTE_POSTGRES_SSLMODE = "prefer"
 
 LOCAL_UPLOADS_DIR = "/home/zulip/uploads"

compose.yaml
@@ -90,8 +90,7 @@ services:
       - zulip__email_password
     environment:
       # Default hostnames; configure your application in compose.override.yaml
-      DB_HOST: "database"
-      DB_HOST_PORT: "5432"
+      SETTING_REMOTE_POSTGRES_HOST: "database"
       SETTING_MEMCACHED_LOCATION: "memcached:11211"
       SETTING_RABBITMQ_HOST: "rabbitmq"
       SETTING_REDIS_HOST: "redis"

entrypoint.sh
@@ -40,10 +40,10 @@ normalize_bool() {
 ## Settings
 
 # PostgreSQL
-DB_HOST="${DB_HOST:-127.0.0.1}"
-DB_HOST_PORT="${DB_HOST_PORT:-5432}"
+SETTING_REMOTE_POSTGRES_HOST="${SETTING_REMOTE_POSTGRES_HOST:-127.0.0.1}"
 DB_NAME="${DB_NAME:-zulip}"
 DB_USER="${DB_USER:-zulip}"
+SETTING_REMOTE_POSTGRES_SSLMODE="${SETTING_REMOTE_POSTGRES_SSLMODE:-prefer}"
 
 # RabbitMQ
 SETTING_RABBITMQ_HOST="${SETTING_RABBITMQ_HOST:-127.0.0.1}"
@@ -82,7 +82,7 @@ AUTO_BACKUP_INTERVAL="${AUTO_BACKUP_INTERVAL:-30 3 * * *}"
 # All of the above config vars, minus SETTING_ ones.
 our_vars=(
     DATA_DIR
-    DB_HOST DB_HOST_PORT DB_NAME DB_USER
+    DB_NAME DB_USER
     NGINX_WORKERS NGINX_MAX_UPLOAD_SIZE LOADBALANCER_IPS TRUST_GATEWAY_IP
     CERTIFICATES PROXY_ALLOW_ADDRESSES PROXY_ALLOW_RANGES
     ZULIP_AUTH_BACKENDS QUEUE_WORKERS_MULTIPROCESS
@@ -416,18 +416,6 @@ secretsConfiguration() {
     done
     echo "Zulip secrets configuration succeeded."
 }
-databaseConfiguration() {
-    echo "Setting database configuration ..."
-    setConfigurationValue "REMOTE_POSTGRES_HOST" "$DB_HOST" "string"
-    setConfigurationValue "REMOTE_POSTGRES_PORT" "$DB_HOST_PORT" "integer"
-    if [ -z "${SETTING_REMOTE_POSTGRES_SSLMODE:-}" ]; then
-        # We do this defaulting here, so that we don't false-positive the "you
-        # used a SETTING_" for MANUAL_CONFIGURATION deploys.
-        setConfigurationValue "REMOTE_POSTGRES_SSLMODE" "prefer" "string"
-    fi
-    # The password will be set in secretsConfiguration
-    echo "Database configuration succeeded."
-}
 authenticationBackends() {
     echo "Activating authentication backends ..."
     local FIRST=true
@@ -503,10 +491,15 @@ autoBackupConfiguration() {
 }
 waitingForDatabase() {
     local TIMEOUT=60
+    local DB_HOST
+    DB_HOST=${1:-$(su zulip -c "/home/zulip/deployments/current/scripts/get-django-setting REMOTE_POSTGRES_HOST")}
+    local DB_PORT
+    DB_PORT=${2:-$(su zulip -c "/home/zulip/deployments/current/scripts/get-django-setting REMOTE_POSTGRES_PORT")}
+
     echo "Waiting for database server to allow connections ..."
     local PGPASSWORD
     PGPASSWORD="$(crudini --get /etc/zulip/zulip-secrets.conf secrets postgres_password)"
-    while ! PGPASSWORD="$PGPASSWORD" /usr/bin/pg_isready -h "$DB_HOST" -p "$DB_HOST_PORT" -U "$DB_USER" -t 1 >/dev/null 2>&1; do
+    while ! PGPASSWORD="$PGPASSWORD" /usr/bin/pg_isready -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -t 1 >/dev/null 2>&1; do
         if ! ((TIMEOUT--)); then
             echo "Could not connect to database server. Exiting."
             exit 1
@@ -583,7 +576,6 @@ initialConfiguration() {
     if [ "$MANUAL_CONFIGURATION" = "False" ]; then
         # Start with the settings template file.
         cp -a /home/zulip/deployments/current/zproject/prod_settings_template.py /etc/zulip/settings.py
-        databaseConfiguration
         secretsConfiguration
         authenticationBackends
         zulipConfiguration
@@ -593,7 +585,6 @@ initialConfiguration() {
         if [ ! -f "/etc/zulip/settings.py" ] || [ ! -s "/etc/zulip/settings.py" ]; then
             cp -a /home/zulip/deployments/current/zproject/prod_settings_template.py /etc/zulip/settings.py
             # This first time, pull from SETTING_ if provided.
-            databaseConfiguration
             authenticationBackends
             zulipConfiguration
             bootstrapped_from_env=1
@@ -673,8 +664,12 @@ appBackup() {
     fi
     local BACKUP_FOLDER="/tmp/backup-$TIMESTAMP"
     mkdir -p "$BACKUP_FOLDER"
-    waitingForDatabase
-    pg_dump -h "$DB_HOST" -p "$DB_HOST_PORT" -U "$DB_USER" "$DB_NAME" >"$BACKUP_FOLDER/database-postgres.sql"
+    local DB_HOST
+    DB_HOST=$(su zulip -c "/home/zulip/deployments/current/scripts/get-django-setting REMOTE_POSTGRES_HOST")
+    local DB_PORT
+    DB_PORT=$(su zulip -c "/home/zulip/deployments/current/scripts/get-django-setting REMOTE_POSTGRES_PORT")
+    waitingForDatabase "$DB_HOST" "$DB_PORT"
+    pg_dump -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" "$DB_NAME" >"$BACKUP_FOLDER/database-postgres.sql"
     tar -zcvf "$DATA_DIR/backups/backup-$TIMESTAMP.tar.gz" "$BACKUP_FOLDER/"
     rm -r "${BACKUP_FOLDER:?}/"
     echo "Backup process succeeded."
@@ -717,9 +712,13 @@ appRestore() {
         sleep 1
     done
     echo "!! WARNING !! Starting restore process ... !! WARNING !!"
-    waitingForDatabase
+    local DB_HOST
+    DB_HOST=$(su zulip -c "/home/zulip/deployments/current/scripts/get-django-setting REMOTE_POSTGRES_HOST")
+    local DB_PORT
+    DB_PORT=$(su zulip -c "/home/zulip/deployments/current/scripts/get-django-setting REMOTE_POSTGRES_PORT")
+    waitingForDatabase "$DB_HOST" "$DB_PORT"
     tar -zxvf "$DATA_DIR/backups/$BACKUP_FILE" -C /tmp
-    psql -h "$DB_HOST" -p "$DB_HOST_PORT" -U "$DB_USER" "$DB_NAME" <"/tmp/$(basename "$BACKUP_FILE" | cut -d. -f1)/database-postgres.sql"
+    psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" "$DB_NAME" <"/tmp/$(basename "$BACKUP_FILE" | cut -d. -f1)/database-postgres.sql"
     rm -r "/tmp/$(basename "$BACKUP_FILE" | cut -d. -f1)/"
     echo "Restore process succeeded. Exiting."
     exit 0

commit 41d9b8701c21e5a2d7e7fe708286db30070ca4bf

helm: Fix PostgreSQL hostname setting.
This was broken in ca0052022c1623f616838bd64f710a8a8c345d79.

Fixes: #582.

helm/zulip/templates/_helpers.tpl
@@ -66,9 +66,9 @@ include all env variables for Zulip pods
 */}}
 {{- define "zulip.env" -}}
 
-- name: REMOTE_POSTGRES_HOST
+- name: SETTING_REMOTE_POSTGRES_HOST
   value: "{{ template "postgresql.v1.primary.fullname" .Subcharts.postgresql }}"
-- name: REMOTE_POSTGRES_PORT
+- name: SETTING_REMOTE_POSTGRES_PORT
   value: "{{ template "postgresql.v1.service.port" .Subcharts.postgresql }}"
 - name: SETTING_MEMCACHED_LOCATION
   value: "{{ template "common.names.fullname" .Subcharts.memcached }}:11211"